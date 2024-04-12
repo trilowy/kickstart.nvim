78db4be9ac08789e4247a3fb56e5422954c1b866
Trilowy <49493635+trilowy@users.noreply.github.com>
2024-04-12 19:55:08 +0200

    feat: color scheme kanagawa

--- init.lua
+++ init.lua
@@ -766,25 +766,22 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+  -- Color theme
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
+    -- 'folke/tokyonight.nvim',
+    -- 'tanvirtin/monokai.nvim',
+    -- 'sainnhe/sonokai',
+    -- 'glepnir/zephyr-nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.colorscheme 'kanagawa-wave' --'tokyonight-moon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-
-  { 'tanvirtin/monokai.nvim' },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
